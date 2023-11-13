9d4d2654cfc81cc8fd16967c9929bebbc1b5e380
l4te-fl4ker <136440740+l4te-fl4ker@users.noreply.github.com>
2023-11-13 22:02:20 +1300

    Added hello.html chain route pathway

--- server.js
+++ server.js
@@ -19,8 +19,17 @@ app.get('/old-page(.html)?', (req, res) => {
     res.redirect(301, '/new-page.html'); // 302 by default
 });
 
+// Route handlers
+
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempted to load hello.html');
+    next()
+}, (req, res) => {
+    res.send('Hello World!');
+});
+
 app.get('/*', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
