c44a923080a0a920418a5682550022b920c8e95b
l4te-fl4ker <136440740+l4te-fl4ker@users.noreply.github.com>
2023-11-13 21:56:54 +1300

    Initialise 404 route and redirect from old-page to new-page

--- server.js
+++ server.js
@@ -3,14 +3,24 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
 
-app.get('^/$|/index.html', (req, res) => {
+// Express accepts regex expressions for route names
+app.get('^/$|/index(.html)?', (req, res) => {
     // Can be done one of two ways
     // res.sendFile('./views/index.html', { root: __dirname });
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
-app.get('/new-page.html', (req, res) => {
 
+app.get('/new-page.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
 });
+
+// Redirect from an old page to a new page
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page.html'); // 302 by default
+});
+
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
